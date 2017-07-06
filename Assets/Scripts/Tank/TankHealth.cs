74df0113719063895e202252b0959c36deec1c23
josh16 <josh.randall16@gmail.com>
2017-07-06 19:57:58 -0400

    Tank Health
    -Created a UI that represents the tanks health
    -Added tank health script to the tank
    -Set the health UI , OnDeath, and Take damage function for the tank

--- Assets/Scripts/Tank/TankHealth.cs
+++ Assets/Scripts/Tank/TankHealth.cs
@@ -10,7 +10,7 @@ public class TankHealth : MonoBehaviour
     public Color m_ZeroHealthColor = Color.red;    
     public GameObject m_ExplosionPrefab;
     
-    /*
+    
     private AudioSource m_ExplosionAudio;          
     private ParticleSystem m_ExplosionParticles;   
     private float m_CurrentHealth;  
@@ -33,22 +33,45 @@ public class TankHealth : MonoBehaviour
 
         SetHealthUI();
     }
-    */
+    
 
     public void TakeDamage(float amount)
     {
         // Adjust the tank's current health, update the UI based on the new health and check whether or not the tank is dead.
+        m_CurrentHealth -= amount;
+
+        SetHealthUI(); // Call this function to adjust the damage on the slider if hit.
+
+        if(m_CurrentHealth <= 0f && !m_Dead)
+        {
+            OnDeath(); //Call the death function
+        }
+
+
+
     }
 
 
     private void SetHealthUI()
     {
         // Adjust the value and colour of the slider.
+        m_Slider.value = m_CurrentHealth;
+        m_FillImage.color = Color.Lerp(m_ZeroHealthColor,m_FullHealthColor,m_CurrentHealth / m_StartingHealth); //Change the images colour
     }
 
 
     private void OnDeath()
     {
         // Play the effects for the death of the tank and deactivate it.
+        m_Dead = true;
+
+        m_ExplosionParticles.transform.position = transform.position;
+        m_ExplosionParticles.gameObject.SetActive(true);
+        //Play the explosion
+        m_ExplosionParticles.Play();
+        //Play the audio
+        m_ExplosionAudio.Play();
+
+        gameObject.SetActive(false); //Set the tank to false so the player can't move the tank anymore
     }
 }
