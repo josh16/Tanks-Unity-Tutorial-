6fcdc945a1ce50842d7e94da2c508801782a5f7f
Joshua Randall <josh.randall16@gmail.com>
2017-07-10 13:06:23 -0400

    ShellExplosion script
    -Added the shell explosion script
    -Edited the shell prefab game object
    - Inside the script, a for loop has been added to check all rigidbodies
    that were on the layer “m_tankmask” for the explosion physics to affect
    them.

--- Assets/Scripts/Shell/ShellExplosion.cs
+++ Assets/Scripts/Shell/ShellExplosion.cs
@@ -14,13 +14,54 @@ public class ShellExplosion : MonoBehaviour
     private void Start()
     {
         Destroy(gameObject, m_MaxLifeTime);
+
     }
 
 
     private void OnTriggerEnter(Collider other)
     {
         // Find all the tanks in an area around the shell and damage them.
-    }
+
+		//An Array of collders that detect the pos, radius, and layer mask of what type of
+		//game object that is going to collide.
+		Collider[] colliders = Physics.OverlapSphere (transform.position, m_ExplosionRadius, m_TankMask);
+			
+		// Have a for loop to check each rigidbody "The tanks"
+		for (int i = 0; i < colliders.Length; i++) 
+		{
+
+			//Setting the targetRigidbody equal to all the collders with a rigidbody
+			Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>;
+		
+			//If there is nothing there (rigidbody), continue on the next loop.
+			if (!targetRigidbody) 
+			{
+				continue;
+			}
+		
+			//Applying an Explosion force to a rigidbody (The Tanks) to push them away
+		    targetRigidbody.AddExplosionForce (m_ExplosionForce, transform.position, m_ExplosionRadius);
+
+
+			//Damage the tanks health by getting it's component
+			TankHealth targetHealth = targetRigidbody.GetComponent<TankHealth>();
+		
+		
+			if (!targetHealth) 
+			{
+				continue; // if it's not a target health, continue on to the next interation.
+			}
+
+
+			float damage = CalculateDamage (targetRigidbody.position);
+
+		}
+
+
+
+    
+	
+	}
 
 
     private float CalculateDamage(Vector3 targetPosition)
